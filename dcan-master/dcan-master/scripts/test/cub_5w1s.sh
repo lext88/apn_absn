cb1872a6e7c38785af97e9230fa526213e55a9f3
Alexis Tao <86131935+Alexis-Tao88@users.noreply.github.com>
2024-07-18 14:18:55 -0500

    Delete dcan-master/dcan-master directory

--- dcan-master/dcan-master/scripts/test/cub_5w1s.sh
+++
@@ -1,1 +0,0 @@
-python test.py -dataset cub -gpu 1  -temperature_attn 2.0 -lamb 1 -w_p 0.5
--- dcan-master/dcan-master/scripts/train/tieredimagenet_5w1s.sh
+++
@@ -1,1 +0,0 @@
-python train.py -batch 64 -dataset tieredimagenet -gpu 1,0 -extra_dir your_run -temperature_attn 5.0 -lamb 0.25 -milestones 60 80 -max_epoch 100
