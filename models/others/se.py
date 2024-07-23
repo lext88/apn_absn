2647b04710d6cc5890c09930b2567828874e4029
Alexis Tao <86131935+lext88@users.noreply.github.com>
2024-07-22 19:42:10 -0500

    Update se.py

--- models/others/se.py
+++ models/others/se.py
@@ -1,30 +1,21 @@
+import torch
 import torch.nn as nn
-
 
 class SqueezeExcitation(nn.Module):
     def __init__(self, channel, reduction=16):
         super(SqueezeExcitation, self).__init__()
         hdim = 64
-        self.conv1x1_in = nn.Sequential(nn.Conv2d(channel, hdim, kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(hdim),
-                                        nn.ReLU(inplace=False))
-        self.conv1x1_out = nn.Sequential(nn.Conv2d(hdim, channel, kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(channel),
-                                        nn.ReLU(inplace=False))
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
         self.fc = nn.Sequential(
-            nn.Linear(hdim, hdim // reduction, bias=False),
+            nn.Linear(channel, hdim // reduction, bias=False),
             nn.ReLU(inplace=False),
-            nn.Linear(hdim // reduction, hdim, bias=False),
+            nn.Linear(hdim // reduction, channel, bias=False),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        x = self.conv1x1_in(x)
-        b, c, _, _ = x.size()
+        b, c, seq_len = x.size()
         y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
+        y = self.fc(y).view(b, c, 1)
         x = x * y.expand_as(x)
-        x = self.conv1x1_out(x)
         return x
-
