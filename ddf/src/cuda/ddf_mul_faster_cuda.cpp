2d14f2d292407ad7deb65370c15b23ddee08959e
Alexis Tao <86131935+lext88@users.noreply.github.com>
2024-07-31 00:28:59 -0500

    Update ddf_mul_faster_cuda.cpp

--- ddf/src/cuda/ddf_mul_faster_cuda.cpp
+++ ddf/src/cuda/ddf_mul_faster_cuda.cpp
@@ -8,9 +8,8 @@ int DDFMulFasterForwardLauncher(
     const at::Tensor features, const at::Tensor channel_filter,
     const at::Tensor spatial_filter, const int kernel_size,
     const int dilation, const int stride,
-    const int batch_size,const int channels,
-    const int bottom_height, const int bottom_width,
-    const int top_height, const int top_width,
+    const int batch_size, const int sequence_length,
+    const int embedding_dim, const int output_length,
     at::Tensor output);
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x, " must be a CUDA tensor ")
@@ -21,8 +20,8 @@ int DDFMulFasterForwardLauncher(
     CHECK_CONTIGUOUS(x)
 
 int ddf_mul_faster_forward_cuda(
-    at::Tensor features,at::Tensor channel_filter, at::Tensor spatial_filter,
-    int kernel_size, int dilation, int stride, at::Tensor output){
+    at::Tensor features, at::Tensor channel_filter, at::Tensor spatial_filter,
+    int kernel_size, int dilation, int stride, at::Tensor output) {
     CHECK_INPUT(features);
     CHECK_INPUT(channel_filter);
     CHECK_INPUT(spatial_filter);
@@ -30,17 +29,14 @@ int ddf_mul_faster_forward_cuda(
     at::DeviceGuard guard(features.device());
 
     const int batch_size = features.size(0);
-    const int channels = features.size(1);
-    const int bottom_height = features.size(2);
-    const int bottom_width = features.size(3);
-    const int top_height = output.size(2);
-    const int top_width = output.size(3);
+    const int sequence_length = features.size(1);
+    const int embedding_dim = features.size(2);
+    const int output_length = output.size(1);
 
     DDFMulFasterForwardLauncher(features, channel_filter, spatial_filter,
                                 kernel_size, dilation, stride,
-                                batch_size, channels,
-                                bottom_height, bottom_width,
-                                top_height, top_width,
+                                batch_size, sequence_length,
+                                embedding_dim, output_length,
                                 output);
     return 1;
 }
